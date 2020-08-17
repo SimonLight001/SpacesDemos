e9811354c561a0871d6bb116ac1e791d210969d9
Simon Light <sipoe900@gmail.com>
2020-08-17 19:49:41 +0100

    fit web oage to powerpoint

--- Location_COVID/web_server.py
+++ Location_COVID/web_server.py
@@ -7,19 +7,33 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def main_page():
     global people
+    maxSafe = 580
     if request.method == 'GET':
         app.logger.info(people)
-        status = 'red'
-        if int(people) < 580:
-            status = 'green'
+        percentage = round(100 * (int(people) / int(maxSafe)))
+        color = 'rgb(235, 97, 52)'
+        status = 'Not Safe'
+        if int(people) < maxSafe:
+            status = 'Safe'
+            color = 'rgb(85,183,75)'
         return '''
         <html>
+         <meta http-equiv="refresh" content="5" />
             <head>
-                <title>Library</title>
+                <title>Library COVID Tracker</title>
             </head>
-            <body style="margin: auto;width: 50%; background-color:''' + status + '''">
-                <h1 style="font-size: large">Library</h1>
-                <h1>''' + str(people) + '''</h1>
+            <body style="color: white; font-family: Futura; text-align: center; background-color:''' + color + '''">
+                <div style="display: inline-block; width: 100%; white-space: nowrap;">
+                    <h1 style="text-align: left; padding-left: 10px; font-size: 50px"> Library </h1>
+                    <h1 style="text-align: right; padding-right: 10-px; font-size: 200px">''' + str(percentage) + '''%</h1>
+                    <h1 style="font-size: 50px">''' + status + '''</h1>
+                </div>
+                <div style="background-color: white; padding: 10px; color: black">
+                    <p style="text-align: left"> Current Occupancy </p>
+                    <p style="text-align: right"> Maximum Safe Occupancy </p>
+                    <h1 style="text-align: left">''' + str(people) + '''</h1>
+                    <h1 style="text-align: right">''' + str(maxSafe) + '''</h1>
+                </div>
             </body>
         </html>'''
     elif request.method == 'POST':
